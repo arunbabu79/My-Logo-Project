c1b805a7c108211905240356de81b319dfef8531
akannupa <akannupa@HEN018448.am.elcompanies.net>
2021-06-08 14:59:55 -0400

    Retry updated for runner

---
+++ yourlogo/src/test/java/parallel/TestRunner.java
@@ -0,0 +1,25 @@
+package parallel;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		 //features = "/src/test/resources/parallel/",
+		 glue={"parallel"},
+		 plugin = {"pretty",
+				   "html:Report/cucumber-html-report.html", 
+				   "json:Report/cucumber-json-report.json",
+				   "junit:Report/cucumber-junit-report.xml",
+				   "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				   "rerun:target/rerun.txt"
+				    }, //to generate different report types
+		dryRun=false,
+		tags="@smoke",
+		monochrome = true
+		 )
+
+public class TestRunner {
+	
+}
